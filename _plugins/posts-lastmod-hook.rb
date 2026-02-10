2129f0c37a8432ed5a1162d7dac7a4ec91a36751
zhuhaiyi <zhuhaiyi@ksztone.com>
2026-02-10 10:55:38 +0800

    Revert "build fail"
    This reverts commit 8e78d881a169d2fe33d70abd3e2f00f9a1262a85.

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -3,9 +3,12 @@
 # Check for changed posts
 
 Jekyll::Hooks.register :posts, :post_init do |post|
-  next unless post.path
-  commit_num = `git rev-list --count HEAD "#{ post.path }" 2>/dev/null`.strip
-  next if commit_num.empty? || commit_num.to_i <= 1
-  lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }" 2>/dev/null`.strip
-  post.data['last_modified_at'] = lastmod_date if lastmod_date && !lastmod_date.empty?
+
+  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+
+  if commit_num.to_i > 1
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+    post.data['last_modified_at'] = lastmod_date
+  end
+
 end
